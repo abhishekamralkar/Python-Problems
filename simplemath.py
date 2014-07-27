5c94210146aa0eda1fd600322fdcc2e8888ed62a
Abhishek Amralkar <abhishekanand1984@gmail.com>
2014-07-27 10:42:50 +0530

    Simple Mathematics in Python

---
+++ simplemath.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+#Every programmin language provides some kind of way of doing numbers and math.
+#Simple Mathematics
+# + Addition
+# - Subtraction
+# / Division
+# * Multiplication
+# % Remainder
+# < Less Than
+# > Greater Than
+# <= Less Than Equal
+# >= Greater Than Equal
+
+print " Lets do some mathematics"
+
+print "Bananas", 100 + 2 / 6
+print "Mangoes", 50000 - 44444 * 29 % 6
+
+
+
