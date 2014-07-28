fb392aa9de14e1636f8e357b6f3f20bb2419b99c
Abhishek Amralkar <abhishekanand1984@gmail.com>
2014-07-28 18:56:44 +0530

    String Variable in Python

---
+++ variable_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+my_name = 'Abhishek Anand Amralkar'
+my_age = 29
+my_height = 74 #inches
+my_weight = 85 #KG
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall" % my_height
+print "He's %d KG" % my_weight
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the tea." % my_teeth
+
+print "If I add %d, %d and %d I get %d." % ( my_age, my_height, my_weight, my_age + my_height + my_weight)
