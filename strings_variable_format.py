50f84680d00a021cd17f728ea6b03063269b243f
Abhishek Amralkar <abhishekanand1984@gmail.com>
2014-07-28 21:18:25 +0530

    Strings,Variables and Format in Python

---
+++ strings_variable_format.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+x = "There are %d types of people." % 10
+binary = "binary"
+do_not = "don't"
+y = "Those who know %s and those who %s." % (binary, do_not)
+
+print x
+print y
+
+print "I said: %r." % x
+print "I also said: '%s'." % y
