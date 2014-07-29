e2ba4b8cfa2e05b183dde4ab97784c1628c2553c
Abhishek Amralkar <abhishekanand1984@gmail.com>
2014-07-29 21:54:41 +0530

    Formatting in Python

--- formatting.py
+++ formatting.py
@@ -12,4 +12,25 @@ print formatter % (
      "I had this thing.",
      "That you could right up time",
      "but it did't sing",
-     "SO I said goodnight.")
+     "So I said goodnight.")
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+
+print "Here are the days:", days
+print "Here are the months:", months
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm spilt\non a line."
+backlash_cat = "I'm \\ a \\ cat"
+
+fat_cat = """
+I'll do a list:
+\t* Cat Food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backlash_cat
+print fat_cat
