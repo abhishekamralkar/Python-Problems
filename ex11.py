7284cc8a04d7d5032a839621b4a3f1cf50328e06
Abhishek Amralkar <abhishekanand1984@gmail.com>
2014-07-31 22:40:33 +0530

    script

--- ex11.py
+++ ex11.py
@@ -7,3 +7,10 @@ print "How much do you weigh?",
 weight = raw_input()
 
 print "So you're %r old, %r tall and %r heavy." % ( age, height, weight)
+
+
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+weight = raw_input(" How much you weight? ")
+
+print "So you are %r old, %r tall and %r heavy." % (age, height, weight)
