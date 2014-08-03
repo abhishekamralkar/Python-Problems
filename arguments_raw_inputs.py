dfb87586dcf23e062b2cfd643098c06b5efd8301
Abhishek Amralkar <abhishekanand1984@gmail.com>
2014-08-03 16:49:54 +0530

    Arguments and Raw Input

---
+++ arguments_raw_inputs.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+from sys import argv
+
+script, user_name = argv
+
+prompt = '>'
+
+print "Hi %s, I'm the %s script." % (user_name, script)
+print "I'd like to ask you a few questions."
+print "Do you like me %s?" % user_name
+likes = raw_input(prompt)
+print "Where do you live %s?" % user_name
+lives = raw_input(prompt)
+
+print "What kind of computer do yuo have?"
+computer=raw_input(prompt)
+
+print """
+
+Alright, so you said %r about liking me.
+You live in %r. Not sure where that is.
+And you have a %r computer. NIce.
+""" % (likes, lives, computer)
+
+
