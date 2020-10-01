e2f1d8e499c4ea251df7d019a1eceefadf3fba53
Tanishbansal2011 <72216510+Tanishbansal2011@users.noreply.github.com>
2020-10-01 22:48:42 +0530

    Update print.py

--- print.py
+++ print.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-print "Hello World"
-print "Hello Again"
-print "I like this typing"
+print "Hello World"*10
+print "Hello Again"*10
+print "I like this typing"*10
 print "This is fun"
 print 'Yay! Printing'
 print "I'd much rather you 'not'."
